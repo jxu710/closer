0c6bcd557e177242fcdfa820691c439cd5833fae
Jingru Xu <nicoxu1000@gmail.com>
2023-08-03 11:42:09 -0400

    change loading time from 3000ms to 1000ms

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 // import components styles
-import './App.scss';
+import "./App.scss";
 
-import React, { useEffect, useState } from 'react';
-import { Redirect, Route, RouteComponentProps } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Redirect, Route, RouteComponentProps } from "react-router-dom";
 
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 
 // import components
-import SignIn from './components/splash/SignIn';
-import SignUp from './components/splash/SignUp';
-import Splash from './components/splash/Splash';
+import SignIn from "./components/splash/SignIn";
+import SignUp from "./components/splash/SignUp";
+import Splash from "./components/splash/Splash";
 // Get stores
-import { useStore } from './features/store';
+import { useStore } from "./features/store";
 // Custom hooks
-import { useTimeout } from './hooks/useTimeOut';
-import CycleMainPage from './pages/CycleMainPage';
+import { useTimeout } from "./hooks/useTimeOut";
+import CycleMainPage from "./pages/CycleMainPage";
 // import pages
-import Dashboard from './pages/Dashboard';
+import Dashboard from "./pages/Dashboard";
 
-import DesktopQRCode from './components/DeskTopQRCode';
-import useWindowResize from './hooks/useWindowSize';
+import DesktopQRCode from "./components/DeskTopQRCode";
+import useWindowResize from "./hooks/useWindowSize";
 
 setupIonicReact();
 
@@ -28,32 +28,33 @@ const App: React.FC<RouteComponentProps> = () => {
   const userStatus = useStore((state) => state.userStatus);
   const [loading, setLoading] = React.useState<boolean>(true);
 
-  useTimeout(() => setLoading(false), 3000);
+  useTimeout(() => setLoading(false), 1000);
   // use window size to check if has been resized
   const [width, height] = useWindowResize();
 
   const [isMobile, setIsMobile] = useState(false);
-  const [qrCodeUrl, setQrCodeUrl] = useState('');
+  const [qrCodeUrl, setQrCodeUrl] = useState("");
 
   const checkDevice = () => {
-      if (navigator.userAgent.match(/Android/i)
-          || navigator.userAgent.match(/webOS/i)
-          || navigator.userAgent.match(/iPhone/i)
-          || navigator.userAgent.match(/iPad/i)
-          || navigator.userAgent.match(/iPod/i)
-          || navigator.userAgent.match(/BlackBerry/i)
-          || navigator.userAgent.match(/Windows Phone/i)
-      ) {
-          setIsMobile(true);
-      } else {
-          setIsMobile(false);
-          setQrCodeUrl(window.location.href);
-      }
-  }
+    if (
+      navigator.userAgent.match(/Android/i) ||
+      navigator.userAgent.match(/webOS/i) ||
+      navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/iPod/i) ||
+      navigator.userAgent.match(/BlackBerry/i) ||
+      navigator.userAgent.match(/Windows Phone/i)
+    ) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+      setQrCodeUrl(window.location.href);
+    }
+  };
 
   // if window size has been changed, check if it is mobile or not
   useEffect(() => {
-      checkDevice();
+    checkDevice();
   }, [width, height]);
 
   return (
@@ -62,61 +63,64 @@ const App: React.FC<RouteComponentProps> = () => {
         <Splash />
       ) : (
         <>
-            {isMobile ? <IonReactRouter>
-          <IonRouterOutlet>
-            <Route
-              path="/sign-in"
-              render={
-                userStatus === "guest" || userStatus === "loggedIn"
-                  ? () => <Redirect to="/" />
-                  : () => <SignIn />
-              }
-              exact={true}
-            />
+          {isMobile ? (
+            <IonReactRouter>
+              <IonRouterOutlet>
+                <Route
+                  path="/sign-in"
+                  render={
+                    userStatus === "guest" || userStatus === "loggedIn"
+                      ? () => <Redirect to="/" />
+                      : () => <SignIn />
+                  }
+                  exact={true}
+                />
 
-            <Route
-              path="/sign-up"
-              render={
-                userStatus === "registered"
-                  ? () => <Redirect to="/sign-in" />
-                  : () => <SignUp />
-              }
-              exact={true}
-            />
+                <Route
+                  path="/sign-up"
+                  render={
+                    userStatus === "registered"
+                      ? () => <Redirect to="/sign-in" />
+                      : () => <SignUp />
+                  }
+                  exact={true}
+                />
 
-            <Route
-              path="/cycle"
-              render={
-                userStatus === "guest" || userStatus === "loggedIn"
-                  ? () => <CycleMainPage />
-                  : () => <Redirect to="/sign-in" />
-              }
-              exact={true}
-            />
+                <Route
+                  path="/cycle"
+                  render={
+                    userStatus === "guest" || userStatus === "loggedIn"
+                      ? () => <CycleMainPage />
+                      : () => <Redirect to="/sign-in" />
+                  }
+                  exact={true}
+                />
 
-            <Route
-              path="/dashboard/:endOfCycle"
-              render={
-                userStatus === "guest" || userStatus === "loggedIn"
-                  ? () => <Dashboard />
-                  : () => <Redirect to="/sign-in" />
-              }
-              exact={true}
-            />
+                <Route
+                  path="/dashboard/:endOfCycle"
+                  render={
+                    userStatus === "guest" || userStatus === "loggedIn"
+                      ? () => <Dashboard />
+                      : () => <Redirect to="/sign-in" />
+                  }
+                  exact={true}
+                />
 
-            <Route
-              path="/"
-              render={
-                userStatus === "guest" || userStatus === "loggedIn"
-                  ? () => <Dashboard />
-                  : () => <Redirect to="/sign-in" />
-              }
-              exact={true}
-            />
-          </IonRouterOutlet>
-        </IonReactRouter> : <DesktopQRCode url={qrCodeUrl} />}
+                <Route
+                  path="/"
+                  render={
+                    userStatus === "guest" || userStatus === "loggedIn"
+                      ? () => <Dashboard />
+                      : () => <Redirect to="/sign-in" />
+                  }
+                  exact={true}
+                />
+              </IonRouterOutlet>
+            </IonReactRouter>
+          ) : (
+            <DesktopQRCode url={qrCodeUrl} />
+          )}
         </>
-        
       )}
     </IonApp>
   );
